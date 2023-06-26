119e1634b0833b58169a1dca49c4ea345756c70d
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2023-06-26 12:26:19 +0200

    styles and paint poke images

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
+const imageConteiner = document.querySelector('.image-container');
 let imageArray = [];
 
 const getPoke = async() => {
-    for(let i = 1; i <= 10; i++){
+    for(let i = 1; i <= 26; i++){
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
         let data = await response.json();
-        let pokeImg = data.sprites.front_default;
+        let pokeImg = data.sprites.other.home.front_default;
         imageArray.push(pokeImg);
     }
+    let lastElement = imageArray.pop();
+    imageArray.unshift(lastElement);
+    imageArray.map((pokeImage) => {
+        let image = document.createElement("img");
+        image.src = pokeImage;
+        image.classList.add("pokeImage");
+        imageConteiner.appendChild(image);
+    });
 };
 
 getPoke();
-console.log(imageArray);
